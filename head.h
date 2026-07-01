ef8dcfe0cb3b3668fbb5ee23780a6cf11a7aadd4
isaaczanafria <isaaczanafria@gmail.com>
2026-07-01 10:06:28 -0500

    Realice el ejercicio 1

--- head.h
+++ head.h
@@ -1,1 +1,15 @@
 //head
+//EJERCICIO1
+//media aritmética
+double calcularMedia(const double datos[], int n);
+//varianza  
+double calcularVarianza(const double datos[], int n, double media);
+//desviación estándar
+double calcularDesviacionEstandar(double varianza);
+//curtosis
+double calcularCurtosis(const double datos[], int n, double media, double desviacion);
+//máximo y mínimo
+void buscarMaxMin(const double datos[], int n, double& maximo, double& minimo);
+//mostrar resultados
+void mostrarResultados(double media, double varianza, double desviacion, double curtosis, double maximo, double minimo);
+
