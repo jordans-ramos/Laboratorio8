0ce3ef50eeb92b6ed572565c6c743e16ce474fa6
Jordan Ramos <jordansrm112007@gmail.com>
2026-07-01 23:30:46 -0500

    Añadir funciones finales solicitadas

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -9,20 +9,28 @@ calcule el elemento máximo, mínimo, traza, matriz transpuesta
 y una función para comprobar si una matriz es simétrica o no.
 */
 void ejercicio2(){
-	int matriz[10][10];
+	//int matriz[10][10];
 	int n;
 	cout<<"Ingrese el tamaño para la matriz: "<<endl;
 	cin>>n;
 	
-//	int matriz[4][4]={
-//	{1,2,3,4},
-//	{5,6,7,8},
-//	{9,10,11,12},
-//	{13,14,15,16}	
+//	comprobacion de funcion simetrica
+//	int matriz[10][10]={
+//	{1,2,3},
+//	{2,1,7},
+//	{3,7,1}	
 //	};
 	mostrarMatriz(matriz,n);
+	cout<<endl;
 	Maximo(matriz, n);
+	cout<<endl;
 	Minimo(matriz, n);
+	cout<<endl;
+	trazaMatriz(matriz, n);
+	cout<<endl;
+	//transpuesta(matriz, n);
+	Transpuesta(matriz, n);
+	simetrica(matriz, n);
 }
 
 void mostrarMatriz(int arr[10][10], int n){
@@ -36,16 +44,6 @@ void mostrarMatriz(int arr[10][10], int n){
         cout << endl;
     }
 }
-
-//void mostrarMatriz(int arr[10][10], int n){
-//	for(int i =0; i<n; i++){
-//		for(int j=0; j<n; j++){
-//			arr[i++][j++] +=1;
-//			cout<<arr[i][j]<<" ";
-//		}
-//		cout<<endl;
-//	}
-//}
 
 void Maximo(int arr[10][10], int n){
 	int max = arr[0][0];
@@ -73,7 +71,58 @@ void Minimo(int arr[10][10], int n){
 	cout<<"El numero menor del arreglo es: "<<min<<endl;
 }
 
+void trazaMatriz(int arr[10][10], int n){
+	int suma=0;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			if(i==j){
+				suma += arr[i][j];
+			}
+		}
+	}
+	cout<<"La traza de la matriz es: "<<suma<<endl;
+}
 
+void transpuesta(int arr[10][10], int n){
+	int aux;
+	cout<<"Matriz transpuesta: "<<endl;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cout<<arr[j][i]<<" ";
+		}
+		cout<<endl;
+	}
+	
+}
 
+void Transpuesta(int arr[10][10], int n){
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            int aux = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = aux;
+        }
+    }
+	cout<<"Matriz transpuesta: "<<endl;
+	
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
+void simetrica(int arr[10][10], int n){
+	bool simetrica= true;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			if(arr[i][j]!=arr[j][i])
+			simetrica = false;
+			break;
+		}
+	}
+	if(simetrica==true)cout<<"Es simetrica. "<<endl;
+	else cout<<"No es simetrica. "<<endl;
+}
 
--- head.h
+++ head.h
@@ -9,3 +9,11 @@ void ejercicio2();
 void mostrarMatriz(int arr[10][10], int n);
 
 void Minimo(int arr[10][10], int n);
+
+void trazaMatriz(int arr[10][10], int n);
+
+void transpuesta(int arr[10][10], int n);
+
+void Transpuesta(int arr[10][10], int n);
+
+void simetrica(int arr[10][10], int n);
