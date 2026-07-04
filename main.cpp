a747204b433419a45aec621a007cdbc120dc6b36
Jordan Ramos <jordansrm112007@gmail.com>
2026-07-04 09:38:29 -0500

    Cambio en main para el merge

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#include "head.h"
-
-using namespace std;
-
-int main(){
-	ejercicio2();
-	return 0;
-}
+//#include <iostream>
+//#include "head.h"
+//
+//using namespace std;
+//
+//int main(){
+//	ejercicio2();
+//	return 0;
+//}
