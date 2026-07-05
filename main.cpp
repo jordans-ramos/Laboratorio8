3a8d4c8c00d7494ee35c57d7d4bd5216457e5d60
Gianfranco542 <roni.tulcanazo04@gmail.com>
2026-07-05 17:35:10 -0500

    Ejercicio3,3 Roni

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
-#include <iostream>
-#include "head.h"
 
-using namespace std;
 
+<<<<<<< HEAD
 int main() {
    caracterFrecuente();
     return 0;
 }
+=======
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//#include "head.h"
+//
+//using namespace std;
+//
+//int main(){
+//	ejercicio2();
+//	return 0;
+//}
+>>>>>>> c07db38d54608c4aac8440ed3c4dd2e1ca1c936c
