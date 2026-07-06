a3d4ee22d4c3c34e86b6da8c5d68af057d25ba2a
Jordan Ramos <jordansrm112007@gmail.com>
2026-07-05 20:35:22 -0500

    Arreglos en el main para ejecucion de todos los ejercicios

--- head.h
+++ head.h
@@ -1,34 +1,44 @@
 //head
+//EJERCICIO1
+//media aritmética
+double calcularMedia(const double datos[], int n);
+//varianza  
+double calcularVarianza(const double datos[], int n, double media);
+//desviación estándar
+double calcularDesviacionEstandar(double varianza);
+//curtosis
+double calcularCurtosis(const double datos[], int n, double media, double desviacion);
+//máximo y mínimo
+void buscarMaxMin(const double datos[], int n, double& maximo, double& minimo);
+//mostrar resultados
+void mostrarResultados(double media, double varianza, double desviacion, double curtosis, double maximo, double minimo);
+
+//EJERCICIO2
+
+void prueba();
+
+void Maximo(int arr[10][10], int n);
+
+void ejercicio2();
+
+void mostrarMatriz(int arr[10][10], int n);
+
+void Minimo(int arr[10][10], int n);
+
+void trazaMatriz(int arr[10][10], int n);
+
+void transpuesta(int arr[10][10], int n);
+
+void Transpuesta(int arr[10][10], int n);
+
+void simetrica(int arr[10][10], int n);
+
+
+//EJERCICIO 3
 
 #include <string>
 std::string obtenerSiglas(const std::string& texto);
 int stringAEntero(const std::string& texto);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,66 @@
+#include "head.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 
 int main() {
-   caracterFrecuente();
+    int opcion;
+
+    do {
+        cout << "\n========== MENU ==========\n";
+        cout << "1. Ejercicio #1\n";
+        cout << "2. Ejercicio #2\n";
+        cout << "3. Ejercicio #3\n";
+        cout << "0. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:{
+                double datos[] = {12.5, 15.2, 10.8, 14.0, 18.6, 11.4, 13.1};
+    			int n = sizeof(datos) / sizeof(datos[0]);
+    			if (n < 2) {
+    			    cout << "Error-> La cantidad de datos es insuficiente para el analisis" << endl;
+    			    return 1;
+    			}
+    			double media = calcularMedia(datos, n);
+    			double varianza = calcularVarianza(datos, n, media);
+    			double desviacion = calcularDesviacionEstandar(varianza);
+    			double curtosis = calcularCurtosis(datos, n, media, desviacion);
+   				double maximo, minimo;
+    			buscarMaxMin(datos, n, maximo, minimo);
+    			mostrarResultados(media, varianza, desviacion, curtosis, maximo, minimo);
+                break;
+            }
+            case 2:
+                ejercicio2();
+                break;
+            case 3:{
+            	cin.ignore();
+                string textoPrueba = "El primer hombre en pisar la luna fue Neil Amstrong";
+    			string siglasObtenidas = obtenerSiglas(textoPrueba);
+    			cout << " OBTENCION DE SIGLAS" << endl;
+    			cout << "Texto original: " << textoPrueba << endl;
+    			cout << "Siglas: " << siglasObtenidas << endl;
+    			string numeroTexto = "2026";
+    			int numeroEntero = stringAEntero(numeroTexto);
+    			cout << "\n CONVERSION STRING A INT" << endl;
+    			cout << "Cadena original: \"" << numeroTexto << "\"" << endl;
+    			cout << "Entero + 4: " << (numeroEntero + 4) << endl; 
+    			
+    			caracterFrecuente();
+                break;
+            }
+            case 0:
+                cout << "\nSaliendo del programa...\n";
+                break;
+            default:
+                cout << "\nOpcion invalida.\n";
+        }
+
+    } while (opcion != 0);
+
     return 0;
 }
 
@@ -16,26 +75,4 @@ int main() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <iostream>
-//#include "head.h"
-//
-//using namespace std;
-//
-//int main(){
-//	ejercicio2();
-//	return 0;
 
